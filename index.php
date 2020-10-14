f1bec37a73465b4de3fff1be14b27eeb3de82b05
maxvGG <46627994+maxvGG@users.noreply.github.com>
2020-10-14 12:31:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,20 +4,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+        };
+    </style>
 </head>
 <body>
+    <table>
+        <thead>
+            <tr>
+                <th>naam</th>
+                <th>prijs</th>
+            </tr>
+        </thead>
+        <tbody id="products">
+            
+
+        </tbody>
+    </table>
+    
     <script>
-        fetch('./product/read.php')
-        .then(response => response.json())
-        .then(data => {
-            for(let i=0; i<data.length; i++) {
-                let div = document.createElement('div');
-                let textnode = document.createTextNode(data[i].naam);
-                div.appendChild(textnode);
-                document.body.appendChild(div);
-            }
+        fetch("./product/read.php")
+        .then((response) => response.json())
+        .then((data) => {
+        for (let i = 0; i < data.length; i++) {
+            const table = document.getElementById("products");
+            var row = table.insertRow(i);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            cell1.innerHTML = data[i].naam;
+            cell2.innerHTML = data[i].prijs;
+        }
         })
-        .catch(error => console.log('error is ', error));
-    </script>
+        .catch((error) => console.log("error is ", error));
+    </script> 
 </body>
 </html>
